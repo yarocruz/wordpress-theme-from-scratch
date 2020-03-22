34790ecd96bcf0c271064a30c50c3134a19c960d
yarocruz <yarocruz@gmail.com>
2020-03-22 19:01:48 -0400

    fixed link to home page

--- wordpress/wp-content/themes/startwordpress/header.php
+++ wordpress/wp-content/themes/startwordpress/header.php
@@ -31,7 +31,7 @@
     <div class="blog-masthead">
         <div class="container">
             <nav class="blog-nav">
-                <a class="blog-nav-item active" href="#">Home</a>
+                <a class="blog-nav-item active" href="<?php echo get_bloginfo('wpurl');?>">Home</a>
                 <?php wp_list_pages('&title_li='); ?>
             </nav>
         </div>
