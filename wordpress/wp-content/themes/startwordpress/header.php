55dea2b26c11f68f538ff1ba54f43cd84d74f75e
yarocruz <yarocruz@gmail.com>
2020-03-22 19:01:06 -0400

    added wp_list_pages, page template, and sidebar items

--- wordpress/wp-content/themes/startwordpress/header.php
+++ wordpress/wp-content/themes/startwordpress/header.php
@@ -32,10 +32,7 @@
         <div class="container">
             <nav class="blog-nav">
                 <a class="blog-nav-item active" href="#">Home</a>
-                <a class="blog-nav-item" href="#">New features</a>
-                <a class="blog-nav-item" href="#">Press</a>
-                <a class="blog-nav-item" href="#">New hires</a>
-                <a class="blog-nav-item" href="#">About</a>
+                <?php wp_list_pages('&title_li='); ?>
             </nav>
         </div>
     </div>
---
+++ wordpress/wp-content/themes/startwordpress/page.php
@@ -0,0 +1,15 @@
+<?php get_header(); ?>
+
+<div class="row">
+    <div class="col-sm-12">
+
+        <?php 
+            if ( have_posts() ): while (have_posts() ) : the_post(); 
+            get_template_part('content', get_post_format() );
+            endwhile; endif; 
+        ?>
+
+    </div>
+</div>
+
+<?php get_footer(); ?>
